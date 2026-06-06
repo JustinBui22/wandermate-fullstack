6420f21fded0b3b47490b77fe553461fad0ca386
Justin Bui <buidominhquanhp1999@gmail.com>
2026-06-06 15:11:30 +0930

    chore: clean frontend dependency and typecheck issues

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -1,2 +1,1 @@
-export const API_BASE_URL = "http://10.0.2.2:8080/The-Project";
-// export const API_BASE_URL = "http://10.0.2.2:8081/The-Project";
+export const API_BASE_URL = "http://10.0.2.2:8082/The-Project";
